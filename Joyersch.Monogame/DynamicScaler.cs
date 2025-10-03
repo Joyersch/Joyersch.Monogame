79cc5d23edce1f8744ecd34ab7cabf9f79316850
Joy Schnittker <dev@joyersch.de>
2025-10-03 09:24:15 +0200

    Updated DynamicScaler to store initial scaling state

--- Joyersch.Monogame/DynamicScaler.cs
+++ Joyersch.Monogame/DynamicScaler.cs
@@ -1,18 +1,23 @@
+using Joyersch.Monogame.Logging;
+
 namespace Joyersch.Monogame;
 
-public class DynamicScaler
+public sealed class DynamicScaler
 {
     List<IScaleable> _scaleables;
+
+    private float _startingScale = 1;
     public DynamicScaler(Display display)
     {
         _scaleables = [];
         display.OnResize += Apply;
+        _startingScale = display.Scale;
     }
 
     public void Apply(float scale)
     {
         foreach (var scaleable in _scaleables)
-            scaleable.SetScale(scale);
+            scaleable.SetScale(scale / _startingScale);
     }
 
     public void Register(IScaleable scale)
